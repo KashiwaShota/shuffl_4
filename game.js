fdf3c62b37189bbf64813e4479ac63c040ad0629
KashiwaShota <nike07th.kc@gmail.com>
2012-11-01 19:48:09 +0900

    First commit

---
+++ game.js
@@ -0,0 +1,22 @@
+﻿Array.prototype.shuffle = function(){
+  var i = this.length;
+  while(i){
+    var j = Math.floor(Math.random() * i);
+    var t = this[--i];
+    this[i] = this[j];
+    this[j] = t;
+  }
+  return this;
+}
+
+document.write("アルゴリズムとデータ構造：シャッフル演習");
+a = [1, 2, 3, 4, 5];
+document.write("<BR>シャッフル前：");
+for(var k = 0; k < a.length; k++){
+  document.write(a[k] + "、");
+}
+a.shuffle();
+document.write("<BR>シャッフル後：");
+for(var l = 0; l < a.length; l++){
+  document.write(a[l] + "、");
+}
